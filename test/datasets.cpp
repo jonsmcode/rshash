d57248a2ce39d31365c5b4b28d76fe32f9f0b249
Schulte-Mattler <adm_js4718fu@mimir.imp.fu-berlin.de>
2025-06-03 13:19:35 +0200

    random datasets

--- test/datasets.cpp
+++ test/datasets.cpp
@@ -27,17 +27,27 @@ void different_dna(std::vector<seqan3::dna4> &text, std::vector<seqan3::dna4> &d
     }
 }
 
-void guard_kmer(std::vector<seqan3::dna4> &guarded_kmer, std::vector<seqan3::dna4> &kmer, const uint8_t k) {
-    char r = (kmer[0].to_rank() + 1) % 4;
-    seqan3::dna4 d;
-    for(int i=0; i < k; i++)
-        guarded_kmer.push_back(d.assign_rank(r));
-    for(int i=0; i < k; i++)
-        guarded_kmer.push_back(kmer[i]);
-    r = (kmer[k-1].to_rank() + 1) % 4;
-    for(int i=0; i < k; i++)
-        guarded_kmer.push_back(d.assign_rank(r));
+std::vector<seqan3::dna4> sample_dna(std::vector<seqan3::dna4> &text, const uint64_t position, const uint64_t length) {
+    std::vector<seqan3::dna4> sample;
+    for(uint64_t i=0; i < length; i++) {
+        sample.push_back(text[position+i]);
+    }
+    return sample;
 }
+
+// void guard_kmer(std::vector<seqan3::dna4> &text, std::vector<seqan3::dna4> &guarded_kmer, std::vector<seqan3::dna4> &kmer, const uint8_t k) {
+//     char r;
+//     seqan3::dna4 d;
+//     for(int i=0; i < k; i++) {
+//         r = (text[i].to_rank() + 1) % 4;
+//         guarded_kmer.push_back(d.assign_rank(r));
+//     }
+//     for(int i=0; i < k; i++)
+//         guarded_kmer.push_back(kmer[i]);
+//     r = (kmer[k-1].to_rank() + 1) % 4;
+//     for(int i=0; i < k; i++)
+//         guarded_kmer.push_back(d.assign_rank(r));
+// }
 
 
 // void random_positions(std::vector<uint64_t> &positions, const uint64_t n, const uint8_t k, const uint64_t range) {
@@ -68,34 +78,33 @@ void guard_kmer(std::vector<seqan3::dna4> &guarded_kmer, std::vector<seqan3::dna
 //     }
 // }
 
-
-void random_positions(std::vector<uint64_t> &positions, const uint64_t n, const uint8_t k, const uint64_t range) {
-    if(n == 0)
-        return;
-    if(n > range/k) {
-        std::cout << "n too big";
-        return;
-    }
-    uint64_t p = rand() % range;
-    positions.push_back(p);
-    int i = 1;
-    while(i < n) {
-        p = rand() % range;
-        auto it = std::upper_bound(positions.begin(), positions.end(), p) - positions.begin();
-        if(it == 0) {
-            if(labs(p - positions[it]) > k) {
-                positions.insert(positions.begin()+it, p);
-                i++;
-            }
-        }
-        else {
-            if(labs(p - positions[it-1]) > k && labs(p - positions[it]) > k) {
-                positions.insert(positions.begin()+it, p);
-                i++;
-            }
-        }
-    }
-}
+// void random_positions(std::vector<uint64_t> &positions, const uint64_t n, const uint8_t k, const uint64_t range) {
+//     if(n == 0)
+//         return;
+//     if(n > range/k) {
+//         std::cout << "n too big";
+//         return;
+//     }
+//     uint64_t p = rand() % range;
+//     positions.push_back(p);
+//     int i = 1;
+//     while(i < n) {
+//         p = rand() % range;
+//         auto it = std::upper_bound(positions.begin(), positions.end(), p) - positions.begin();
+//         if(it == 0) {
+//             if(labs(p - positions[it]) > k) {
+//                 positions.insert(positions.begin()+it, p);
+//                 i++;
+//             }
+//         }
+//         else {
+//             if(labs(p - positions[it-1]) > k && labs(p - positions[it]) > k) {
+//                 positions.insert(positions.begin()+it, p);
+//                 i++;
+//             }
+//         }
+//     }
+// }
 
 
 void insert_kmer(std::vector<seqan3::dna4> &text, const std::vector<seqan3::dna4> &kmer, const uint64_t position)
@@ -171,77 +180,53 @@ void save_dataset(const std::string file, const std::vector<seqan3::dna4> &text,
 }
 
 
-// void build_dataset(const std::string path, const uint8_t k, const uint64_t textlength,
-//                    const int occurences, const double hitrate, const int querylength)
-// {
-//     std::vector<seqan3::dna4> text;
-//     std::vector<std::vector<seqan3::dna4>> queries;
-//     std::vector<uint64_t> textpositions;
-//     const int no_queries = hitrate*textlength;
-//     const int no_kmers = hitrate*textlength*occurences;
-
-//     random_dna(text, textlength);
-//     random_positions(textpositions, no_kmers, k, textlength-k-1);
-//     std::vector<std::tuple<uint64_t, uint64_t, uint64_t, uint64_t>> positions;
-//     for(int i = 0; i < no_queries; i++) {
-//         std::vector<seqan3::dna4> query;
-//         random_dna(query, querylength);
-//         std::vector<seqan3::dna4> kmer;
-//         random_dna(kmer, k);
-//         std::vector<uint64_t> querypositions;
-//         // random_positions(querypositions, occurences, k, querylength-k-1);
-//         for(int j = 0; j < occurences; j++) {
-//             insert_kmer(text, kmer, textpositions[i*occurences+j]);
-//             // insert_kmer(query, kmer, querypositions[j]);
-//             insert_kmer(query, kmer, 100);
-//             // positions.push_back({i, querypositions[j], 0, textpositions[i*occurences+j]});
-//             positions.push_back({i, 100, 0, textpositions[i*occurences+j]});
-//         }
-//         queries.push_back(query);
-//     }
-
-//     std::string dataset = "random_n" + std::to_string(textlength) + "_o" + std::to_string(no_kmers) + "_k" + std::to_string(k);
-//     std::string file = path + dataset;
-
-//     save_dataset(file, text, queries, positions);
-// }
-
-
 void build_dataset(const std::string path, const uint8_t k, const uint64_t textlength,
-                   const int querylength, const int no_queries)
+                   const int querylength, const double hitrate)
 {
     std::vector<seqan3::dna4> text;
     std::vector<std::vector<seqan3::dna4>> queries;
-    std::vector<uint64_t> textpositions;
 
     random_dna(text, textlength);
-    // random_positions(textpositions, no_queries, k, textlength-k-1);
-    uint64_t range = textlength/no_queries;
-    assert(range/2 > 3*k);
-    for(int i = 0; i < no_queries; i++) {
-        textpositions.push_back(i*range+(range/2));
-    }
 
+    const uint64_t hits = hitrate/100*(textlength-k+1);
+    const uint64_t dist = 100/hitrate;
+
+    std::cout << "hits: " << hits << " dist: " << dist << "\n";
+
+    std::vector<uint64_t> textpositions;
+    for(uint64_t i = 0; i < hits; i++) {
+        textpositions.push_back(i*dist);
+    }
     std::vector<std::tuple<uint64_t, uint64_t, uint64_t, uint64_t>> positions;
-    for(int i = 0; i < no_queries; i++) {
+
+    for(uint64_t i = k/dist+1; i < hits-k/dist; i++)
+    {
         std::vector<seqan3::dna4> query;
-        random_dna(query, querylength);
         std::vector<seqan3::dna4> textkmer;
         std::vector<seqan3::dna4> querykmer;
         std::vector<seqan3::dna4> kmer;
-        random_dna(textkmer, 3*k);
+
+        random_dna(query, querylength);
+        uint64_t s = textpositions[i]-k;
+        uint64_t l = 3*k;
+        if(s < 0) {
+            l += s;
+            s = 0;
+        }
+        if(s + 3*k >= textlength) {
+            l -= textlength - s + 3*k;
+        }
+        textkmer = sample_dna(text, s, l);
         different_dna(textkmer, querykmer);
-        random_dna(kmer, k);
-        insert_kmer(textkmer, kmer, k);
+        kmer = sample_dna(textkmer, k, k);
         insert_kmer(querykmer, kmer, k);
-        // guard_kmer(guarded_kmer, kmer, k);
-        insert_kmer(text, textkmer, textpositions[i]-k);
-        insert_kmer(query, querykmer, 100-k);
-        positions.push_back({i, 100, 0, textpositions[i]});
+        insert_kmer(query, querykmer, (querylength-k)/2);
+
         queries.push_back(query);
+        positions.push_back({i, (querylength-k)/2, 0, textpositions[i]});
     }
 
-    std::string dataset = "random_n" + std::to_string(textlength) + "_o" + std::to_string(no_queries) + "_k" + std::to_string(k);
+    std::string dataset = "random_n" + std::to_string(textlength) + "_o" + std::to_string(hits) + "_k" + std::to_string(k);
     std::string file = path + dataset;
 
     save_dataset(file, text, queries, positions);
@@ -251,26 +236,17 @@ void build_dataset(const std::string path, const uint8_t k, const uint64_t textl
 
 int main(int argc, char** argv)
 {
-    // const uint64_t textlengths[] = {1000000, 10000000, 100000000, 1000000000};
-    const uint64_t textlengths[] = {10000000, 100000000};
+    const uint64_t textlengths[] = {10000000, 100000000, 1000000000};
+    // const uint64_t textlengths[] = {10000000, 100000000};
     const uint8_t k = 31;
+    const double hitrates[] = {0.1, 1, 10}; // percent
 
-    // int n = 100;
-    // int id = 0;
-    // for(int tl : text_lengths) {
-    //     td::cout << "building random lookup dataset " << id << "\n";
-    //     build_dataset("test/datasets/1-lookup-random/", id, k, tl, k, 1, n);
-    //     id++;
-    // }
-
-    // const int occurences = 1;
-    const int querylength = 1000;
-    // const double hitrate = 0.0001;
-    for(uint64_t textlength : textlengths) {
-        std::cout << "building random dataset with text length " << textlength << "\n";
-        // build_random_dataset("test/datasets/2-stream-random/", id, k, tl, query_length, number_queries, n);
-        // build_dataset("/Users/adm_js4718fu/datasets/synthetic/", k, textlength, occurences, hitrate, querylength);
-        build_dataset("../test/datasets/synthetic/", k, textlength, querylength, textlength/1000);
+    for(const uint64_t textlength : textlengths) {
+        for(const double hitrate : hitrates) {
+            std::cout << "building random dataset with text length " << textlength << " with hitrate " << hitrate << "\n";
+            const int querylength = 3*k+100;
+            build_dataset("../test/datasets/synthetic/", k, textlength, querylength, hitrate);
+        }
     }
 
     return 0;
