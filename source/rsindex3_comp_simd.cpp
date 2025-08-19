7da3428f75362394644c40e263c61f831274cf88
Schulte-Mattler <adm_js4718fu@mimir.imp.fu-berlin.de>
2025-08-19 15:07:30 +0200

    leading zeros for simd mask ebug

--- source/rsindex3_comp_simd.cpp
+++ source/rsindex3_comp_simd.cpp
@@ -703,11 +703,22 @@ inline bool lookup_avx512(std::vector<uint64_t> &array, uint64_t query, uint64_t
         __mmask8 mask2 = _mm512_cmpeq_epi64_mask(v, qrv);
         if(mask1) {
             size_t last_found = i + static_cast<size_t>(__builtin_clz(mask1));
+            size_t last_found2;
+            for(int j=0; j < 8; ++j)
+                if(mask1[j] == 1)
+                    last_found2 = i + j;
+            if(last_found != last_found2)
+                std::cout << "intr: " << last_found << " serial: " << last_found2 << std::endl;
             forward = true;
             return true;
         }
         if(mask2) {
             size_t last_found = i + static_cast<size_t>(__builtin_clz(mask2));
+            for(int j=0; j < 8; ++j)
+                if(mask1[j] == 1)
+                    last_found2 = i + j;
+            if(last_found != last_found2)
+                std::cout << "intr: " << last_found << " serial: " << last_found2 << std::endl;
             forward = false;
             return true;
         }
