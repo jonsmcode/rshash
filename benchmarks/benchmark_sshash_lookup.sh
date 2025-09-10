82d840c6a8701115f75b454369f3af4520eac146
Schulte-Mattler <adm_js4718fu@mimir.imp.fu-berlin.de>
2025-09-10 13:26:06 +0200

    bench lookup

--- benchmarks/benchmark_sshash_lookup.sh
+++ benchmarks/benchmark_sshash_lookup.sh
@@ -16,13 +16,14 @@ run()
   do
     BASENAME=$(echo "$f" | sed 's/\.fa.gz$//')
     k=$(echo "${BASENAME##*k}" | grep -o '[0-9]*')
-    if [[ "$BASENAME" == *"bacterial"* || "$BASENAME" == *"human"* || "$BASENAME" == *"cod"* || "$BASENAME" == *"kestrel"* ]]; then
+    # if [[ "$BASENAME" == *"bacterial"* || "$BASENAME" == *"human"* || "$BASENAME" == *"cod"* || "$BASENAME" == *"kestrel"* ]]; then
+    if [[ "$BASENAME" == *"bacterial"* ]]; then
         if [ "$k" -le 32 ]; then
 
           length=$(python3 -c "import gzip; print(sum(len(line.strip()) for line in gzip.open('$f') if not line.startswith(b'>')))")
 
           ms=()
-          for ((i=5; i>=5; i--)); do
+          for ((i=5; i>=1; i--)); do
               m=$(echo "l($length)/l(4)+$i" | bc -l)
               m=$(printf "%.0f" "$m")
               ms+=("$m")
@@ -63,30 +64,30 @@ run()
 
                 echo $f >> $LOG
 
-                # /usr/bin/time -v -o timess.txt $PROGRAM build -i "$f" -o "${BASENAME}.index" -k $k -m $m > sshash_out.txt 2>&1
+                /usr/bin/time -v -o timess.txt $PROGRAM build -i "$f" -o "${BASENAME}.index" -k $k -m $m > sshash_out.txt 2>&1
 
-                # cat sshash_out.txt >> $LOG
+                cat sshash_out.txt >> $LOG
 
-                # file_size=$(stat -c%z "${BASENAME}.index")
-                # buildtime=$(grep "User time" timess.txt | awk -F': ' '{print $2}')
-                # buildmem=$(grep "Maximum resident set size" timess.txt | awk -F': ' '{print $2}')
-                # num_super_kmers=$(grep "^num_super_kmers" sshash_out.txt | awk '{print $2}')
-                # space=$(grep "total:" sshash_out.txt | sed -E 's/^ *total: *([0-9.eE+-]+).*/\1/')
-                # space_o=$(grep "offsets:" sshash_out.txt | sed -E 's/^ *offsets: *([0-9.eE+-]+).*/\1/')
-                # space_m=$(grep "minimizers:" sshash_out.txt | sed -E 's/^ *minimizers: *([0-9.eE+-]+).*/\1/')
-                # bits_key=$(awk '/minimizers:/ {for(i=1;i<=NF;i++) if($i ~ /\[bits\/key\]/) print $(i-1)}' sshash_out.txt | tr -d '(')
+                file_size=$(stat -c%z "${BASENAME}.index")
+                buildtime=$(grep "User time" timess.txt | awk -F': ' '{print $2}')
+                buildmem=$(grep "Maximum resident set size" timess.txt | awk -F': ' '{print $2}')
+                num_super_kmers=$(grep "^num_super_kmers" sshash_out.txt | awk '{print $2}')
+                space=$(grep "total:" sshash_out.txt | sed -E 's/^ *total: *([0-9.eE+-]+).*/\1/')
+                space_o=$(grep "offsets:" sshash_out.txt | sed -E 's/^ *offsets: *([0-9.eE+-]+).*/\1/')
+                space_m=$(grep "minimizers:" sshash_out.txt | sed -E 's/^ *minimizers: *([0-9.eE+-]+).*/\1/')
+                bits_key=$(awk '/minimizers:/ {for(i=1;i<=NF;i++) if($i ~ /\[bits\/key\]/) print $(i-1)}' sshash_out.txt | tr -d '(')
 
-                # parent_dir=$(dirname "$f")
-                # file_name=$(basename "$f")
+                parent_dir=$(dirname "$f")
+                file_name=$(basename "$f")
 
-                # /usr/bin/time -v -o timess.txt $PROGRAM bench -i "${BASENAME}.index" > sshash_out.txt 2>&1
+                /usr/bin/time -v -o timess.txt $PROGRAM bench -i "${BASENAME}.index" > sshash_out.txt 2>&1
 
-                # cat sshash_out.txt >> $LOG
+                cat sshash_out.txt >> $LOG
 
-                # poslookuptime=$(grep '^avg_nanosec_per_positive_lookup ' sshash_out.txt | cut -d' ' -f2)
-                # neglookuptime=$(grep '^avg_nanosec_per_negative_lookup ' sshash_out.txt | cut -d' ' -f2)
+                poslookuptime=$(grep '^avg_nanosec_per_positive_lookup ' sshash_out.txt | cut -d' ' -f2)
+                neglookuptime=$(grep '^avg_nanosec_per_negative_lookup ' sshash_out.txt | cut -d' ' -f2)
                     
-                # echo "$f,$k,$m,$buildtime,$buildmem",$file_size,$space_o,$space_m,$bits_key,$num_super_kmers,$space,$neglookuptime,$poslookuptime >> "$CSV"
+                echo "$f,$k,$m,$buildtime,$buildmem",$file_size,$space_o,$space_m,$bits_key,$num_super_kmers,$space,$neglookuptime,$poslookuptime >> "$CSV"
 
           done
 
