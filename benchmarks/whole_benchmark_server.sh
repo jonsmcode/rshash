ed5733576036af744131d83675f441b00987b1ce
Jonas Schulte-Mattler <js4718fu@augustus.imp.fu-berlin.de>
2025-11-28 12:58:26 +0100

    towards cleanup

--- benchmarks/whole_benchmark_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-./benchmark_synthetic_server.sh
-./benchmark_sshash_synthetic_server.sh
-
--- benchmarks/whole_server_benchmark.sh
+++
@@ -1,4 +0,0 @@
-./benchmark_our_lookup_server.sh
-./benchmark_our_comp_lookup_server.sh
-./benchmark_our_lookup2_server.sh
-./benchmark_our_comp_lookup2_server.sh
